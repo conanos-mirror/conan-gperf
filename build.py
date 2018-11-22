4c6e9a1d04d10bbb62ba5f4a9edbaacfc2adaf13
cppbitman <daihongjun@kedacom.com>
2018-11-21 22:15:12 -0800

    fix linux build

--- build.py
+++ build.py
@@ -1,8 +1,16 @@
 
-from conan.packager import ConanMultiPackager
+#from conan.packager import ConanMultiPackager
+#
+#
+#if __name__ == "__main__":
+#    builder = ConanMultiPackager()
+#    builder.add_common_builds(shared_option_name="gperf:shared", pure_c=True)
+#    builder.run()
 
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="gperf:shared", pure_c=True)
+
+    builder = build_template_default.get_builder(pure_c=True)
+
     builder.run()
